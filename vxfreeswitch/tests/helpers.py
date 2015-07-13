662ea820ce95ec08cc962317f7dd39b3f6a8a7cc
Rudi Giesler <rudi@praekelt.com>
2015-07-13 10:20:44 +0200

    Add helper for sending hangup complete event.

--- vxfreeswitch/tests/helpers.py
+++ vxfreeswitch/tests/helpers.py
@@ -4,6 +4,8 @@ from uuid import uuid4
 
 from zope.interface import implements
 
+import time
+
 from twisted.internet import reactor
 from twisted.internet.defer import (
     inlineCallbacks, returnValue, DeferredQueue, Deferred)
@@ -228,6 +230,17 @@ class FakeFreeSwitchProtocol(LineReceiver):
 
     def sendChannelHangupEvent(self):
         self.sendPlainEvent('Channel_Hangup')
+
+    def sendChannelHangupCompleteEvent(self, duration):
+        """
+        Sends a hangup complete event. Duration = duration of call in ms
+        """
+        hangup_time = int(time.time() * 1000)
+        answer_time = int(hangup_time - duration)
+        self.sendPlainEvent('Channel_Hangup_Complete', {
+            'Caller-Channel-Answered-Time': answer_time,
+            'Caller-Channel-Hangup-Time': hangup_time
+        })
 
     def sendDtmfEvent(self, digit):
         self.sendPlainEvent('DTMF', {
