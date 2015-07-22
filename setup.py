7779000a3b079251e521666996a2f946199987d3
Rudi Giesler <rudi@praekelt.com>
2015-07-22 11:05:00 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vxfreeswitch",
-    version="0.1.6",
+    version="0.1.7a",
     url='http://github.com/praekelt/vumi-freeswitch-esl',
     license='BSD',
     description="A Freeswitch eventsocket transport for Vumi.",
--- vxfreeswitch/__init__.py
+++ vxfreeswitch/__init__.py
@@ -2,6 +2,6 @@
 
 from .voice import VoiceServerTransport
 
-__version__ = "0.1.6"
+__version__ = "0.1.7a"
 
 __all__ = ['VoiceServerTransport']
